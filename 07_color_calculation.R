b7d02a156d9f92b137c8a1552daedf921cef5ed4
Sergey Samsonau <s.samsonau@gmail.com>
2018-03-30 17:20:41 -0400

    no browser

--- 07_color_calculation.R
+++ 07_color_calculation.R
@@ -1,5 +1,5 @@
 output$color_plot <- renderPlot({
-  browser()
+  #browser()
   w_range <- range(finalSpectrum()$wavelength)
   if(w_range[1] > 400 | w_range[2] < 700){
     values$final_color <- "NOT ENOUGTH DATA: spectrum should inlclude at least range 400-700 nm"
@@ -44,7 +44,7 @@ output$color_plot <- renderPlot({
                               uv[1], ", v = ", uv[2])
   #values$final_color <- rgb(XYZ2RGB(XYZ/sum(XYZ)))
   #points(x = uv[1], y = uv[2], col = rgb(XYZ2RGB(XYZ/sum(XYZ))), pch = 19, lw = 9) # rescale to have full intensity of color. Otherwise it may be just dark
-  points(x = uv[1], y = uv[2], pch = 19, lw = 9) # rescale to have full intensity of color. Otherwise it may be just dark
+  points(x = uv[1], y = uv[2], pch = 0) # rescale to have full intensity of color. Otherwise it may be just dark
   
 })
 
